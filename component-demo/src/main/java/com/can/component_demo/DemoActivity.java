f8039f18b38f07dd8c42a52fac41a5eeb62c875a
xuwenneng <xuwenneng@huazhen.com>
2018-08-24 17:38:21 +0800

    初识组件化-简单搭建component

---
+++ component-demo/src/main/java/com/can/component_demo/DemoActivity.java
@@ -0,0 +1,14 @@
+package com.can.component_demo;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CAN on 18-8-24.
+ */
+
+public class DemoActivity extends AppCompatActivity{
+
+
+
+}
