2cab432249e46ba5ad01f0d560d7ebf361bf7acd
xuwenneng <xuwenneng@huazhen.com>
2018-09-17 19:40:47 +0800

    依赖compile改为implementation、api

---
+++ eventbus/src/main/java/com/can/eventbus/ui/TestActivity.java
@@ -0,0 +1,15 @@
+package com.can.eventbus.ui;
+
+import com.can.eventbus.R;
+import com.can.mvp.base.BaseActivity;
+
+/**
+ * Created by CAN on 18-9-17.
+ */
+
+public class TestActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_test;
+    }
+}
