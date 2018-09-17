475e4212ab29d49085071fe3d6b6632bbd367a5f
xuwenneng <xuwenneng@huazhen.com>
2018-09-17 11:14:37 +0800

    build

---
+++ notification/src/main/java/com/can/notification/ui/TestActivity.java
@@ -0,0 +1,15 @@
+package com.can.notification.ui;
+
+import com.can.mvp.base.BaseActivity;
+
+/**
+ * Created by CAN on 18-9-17.
+ */
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+}
