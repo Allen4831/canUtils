f248ef1d37f91aff8f2d5034a79a8ea9ca6272ba
can <13611414180@163.com>
2019-08-28 11:01:50 +0800

    kotlin协程

--- kotlin/src/main/kotlin/com/can/kotlin/ui/TestActivity.kt
+++ kotlin/src/main/kotlin/com/can/kotlin/ui/TestActivity.kt
@@ -1,15 +1,36 @@
 package com.can.kotlin.ui
 
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.can.kotlin.R
 import com.can.kotlin.myClass.Person
+import com.can.mvp.base.BaseActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * Created by CAN on 18-9-27.
  */
-class TestActivity : AppCompatActivity() {
+class TestActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_test
+    }
+
+    init {
+        test()
+    }
+
 
     private fun test(){
         val person = Person()
+
+        GlobalScope.launch {
+            delay(1000)
+            Log.i("TestActivity","delay")
+        }
+
+        Log.i("TestActivity","test")
 
     }
 
