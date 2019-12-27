c16430af27c0628f462a98b98271de92c17e1eae
can <13611414180@163.com>
2019-12-27 09:49:30 +0800

    模拟灯光

--- kotlin/src/main/kotlin/com/can/kotlin/uitls/Main.kt
+++ kotlin/src/main/kotlin/com/can/kotlin/uitls/Main.kt
@@ -20,22 +20,21 @@ private val mItemBank = mapOf("将要进行夜间行驶" to nearLight
 private val mQuestion = arrayOf("将要进行夜间行驶", "夜间通过急弯", "夜间通过坡路", "夜间通过拱桥", "夜间通过人行横道"
         , "进入照明良好道路", "进入照明不良道路", "夜间在路边临时停车", "夜间与机动车会车", "夜间超越前方车辆", "夜间通过路口", "同方向近距离跟车行驶")
 
+fun main() {
+    runBlocking {
+        while (true) {
+            val position = Random().nextInt(mQuestion.size)
+            val question = mQuestion[position]
+            println(question)
+            val scanner = Scanner(System.`in`)
+            print("输入你的答案:")
 
-fun main() = runBlocking {
-    while (true) {
-        val position = Random().nextInt(mQuestion.size)
-        val question = mQuestion[position]
-        println(question)
-        val scanner = Scanner(System.`in`)
-        print("输入你的答案:")
-
-        when (scanner.next()) {
-            "exit" -> {
-                return@runBlocking
+            when (scanner.next()) {
+                "exit" -> return@runBlocking
+                mItemBank[question] -> println("回答正确")
+                else -> println("回答错误，正确答案是:${mItemBank[question]}")
             }
-            mItemBank[question] -> println("回答正确")
-            else -> println("回答错误，正确答案是:${mItemBank[question]}")
+            println()
         }
-        println()
     }
 }
