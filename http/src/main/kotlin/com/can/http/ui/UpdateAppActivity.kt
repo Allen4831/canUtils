f906f1ebbd04a82b5e4713d6797faf0805737846
can <13611414180@163.com>
2019-12-30 14:41:57 +0800

    module http

---
+++ http/src/main/kotlin/com/can/http/ui/UpdateAppActivity.kt
@@ -0,0 +1,14 @@
+package com.can.http.ui
+
+import com.can.http.R
+import com.can.mvp.base.BaseActivity
+
+/**
+ * Created by CAN on 2019/12/30.
+ */
+class UpdateAppActivity : BaseActivity(){
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_activity_update_app
+    }
+}
